591cb19716221021a373248755b477da189429c7
Richard Whiting <richwhiting@yahoo.com>
2021-11-09 09:58:05 -0800

    Checkstyle fixes

--- src/main/java/BearWorkshopInterface.java
+++ src/main/java/BearWorkshopInterface.java
@@ -3,9 +3,13 @@ package main.java;
 public interface BearWorkshopInterface {
     
     public double getCost(Bear bear);
+    
     public boolean addBear(Bear bear);
+    
     public boolean removeBear(Bear bear);
+    
     public double checkout();
+    
     public double calculateTax();
 
 }
